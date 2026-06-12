b480669de392d88ee42c440d226a534304d344f2
Youssef <ya103857@gmail.com>
2026-06-12 16:10:55 +0300

    Add files via upload

---
+++ Python exercise (14).py
@@ -0,0 +1,11 @@
+# إنشاء متغيرات لتخزين البيانات
+# أدخل إسم سيارتك المفضله
+my_car = input("Enter your favorite car: ")
+# أدخل السرعه القصوى للسيارة
+speed_car = int(input("Enter the speed: "))
+# أدخل سنة إصدار السيارة
+model_car = int(input("Enter the model: "))
+
+print (f"Car_name: {my_car}\nMax_speed: {speed_car}\nModel_year:{model_car}")
+
+print("Thank you for using my fun program")
